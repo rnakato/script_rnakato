da566ffdbdb8c204724ec7cf2b342ad5964b1680
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-01-19 21:01:52 +0900

    first commit

---
+++ convert_genename2refFlat.pl
@@ -0,0 +1,26 @@
+#! /usr/bin/perl -w
+
+die "convert_genename2refseq.pl <genefile> <refFlat>\n" if($#ARGV !=1);
+
+$file=$ARGV[0];
+$refflat=$ARGV[1];
+open(ListFile, $refflat) ||die "error: can't open $refflat.\n";
+while(<ListFile>){
+    next if($_ eq "\n");
+    chomp;
+    my @clm = split(/\t/, $_);
+    $Hash{$clm[0]}=$_;
+}
+close (ListFile);
+
+open(ListFile, $file) ||die "error: can't open $file.\n";
+while(<ListFile>){
+    next if($_ eq "\n");
+    chomp;
+    my @clm = split(/\t/, $_);
+    if(exists($Hash{$clm[0]})){
+	print "$Hash{$clm[0]}\n";
+    }
+}
+close (ListFile);
+
