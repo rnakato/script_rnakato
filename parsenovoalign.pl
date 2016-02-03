67280ad42588a6bbcb75964b1fb928f886d5a5d9
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-02-03 19:34:01 +0900

    parsenovoalign.pl

---
+++ parsenovoalign.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+open(File, $ARGV[0]) ||die "error: can't open $ARGV[0].\n";
+while(<File>){
+    next if($_ eq "\n");
+    chomp;
+    if($_ =~ /#\s+Read Sequences:\s+([0-9]+)/){
+	$all=$1;
+    }elsif($_ =~ /#\s+Unique Alignment:\s+([0-9]+) \(\s*(.+)%\)/){
+	$uniq=$1;
+	$runiq=$2;
+    }elsif($_ =~ /#\s+Multi Mapped:\s+([0-9]+) \(\s*(.+)%\)/){
+	$multi=$1;
+	$rmulti=$2;
+    }elsif($_ =~ /#\s+No Mapping Found:\s+([0-9]+) \(\s*(.+)%\)/){
+	$unmap=$1;
+	$runmap=$2;
+    }elsif($_ =~ /#\s+Homopolymer Filter:\s+([0-9]+) \(\s*(.+)%\)/){
+	$homo=$1;
+	$rhomo=$2;
+    }elsif($_ =~ /#\s+Read Length:\s+([0-9]+) \(\s*(.+)%\)/){
+	$read=$1;
+	$rread=$2;
+    }
+}
+print "$all\t$uniq\t$runiq\t$multi\t$rmulti\t$unmap\t$runmap\t$homo\t$rhomo\t$read\t$rread\n";
