8b3ae617314e8cfa22a20752ff593b519213abe9
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-02-05 13:25:09 +0900

    parse_cutadapt.pl

---
+++ parse_cutadapt.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+open(File, $ARGV[0]) ||die "error: can't open $ARGV[0].\n";
+while(<File>){
+    next if($_ eq "\n");
+    chomp;
+    if($_ =~ /Total reads processed:\s+(.+)/){
+	$all=$1;
+    }elsif($_ =~ /Reads with adapters:\s+(.+) \((.+)%\)/){
+	$ad=$1;
+	$rad=$2;
+    }elsif($_ =~ /Reads that were too short:\s+(.+) \((.+)%\)/){
+	$short=$1;
+	$rshort=$2;
+    }elsif($_ =~ /Reads written \(passing filters\):\s+(.+) \((.+)%\)/){
+	$pass=$1;
+	$rpass=$2;
+    }elsif($_ =~ /Total basepairs processed:\s+(.+) bp/){
+	$allbp=$1;
+    }elsif($_ =~ /Total written \(filtered\):\s+(.+) bp \((.+)%\)/){
+	$passbp=$1;
+	$rpassbp=$2;
+    }
+}
+
+print STDERR "Total reads\twith adapters\t%\ttoo short\t%\tpassing filters\t%\tTotal bases\tTotal written\t%\n";
+print "$all\t$ad\t$rad\t$short\t$rshort\t$pass\t$rpass\t$allbp\t$passbp\t$rpassbp\n";
