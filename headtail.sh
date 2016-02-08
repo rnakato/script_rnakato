063c2110afef9a6f6bfe8e3b32d1bde73b7b5065
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-02-08 19:43:53 +0900

    middle.sh

--- headtail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 3 ]; then
-  echo "headtail.sh <file> <start> <end>" 1>&2
-  exit 1
-fi
-
-cat $1 | head -$3 | tail -`expr $3 - $2 + 1`
-
--- middle.sh
+++ middle.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-head -n $3 $1 | tail -n $(($3 - $2 + 1))
+
+if [ $# -ne 3 ]; then
+  echo "middle.sh <file> <start> <end>" 1>&2
+  exit 1
+fi
+
+cat $1 | head -$3 | tail -`expr $3 - $2 + 1`
+
