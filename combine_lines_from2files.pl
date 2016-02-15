aa71f6b610527e41a545eae4d527d85ba15156bf
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-02-15 17:30:28 +0900

    m

--- combine_lines_from2files.pl
+++ combine_lines_from2files.pl
@@ -3,13 +3,14 @@
 use strict;
 use warnings;
 use autodie;
-$file1=$ARGV[0];
-$file2=$ARGV[1];
-$line1=$ARGV[2];
-$line2=$ARGV[3];
+my $file1=$ARGV[0];
+my $file2=$ARGV[1];
+my $line1=$ARGV[2];
+my $line2=$ARGV[3];
 
 die "combine_lines_from2files.pl <file1> <file2> <line1> <line2>\n" if($#ARGV !=3);
 
+my %Hash = {};
 open(List, $file1) ||die "error: can't open $file1.\n";
 while(<List>){
     next if($_ eq "\n");
