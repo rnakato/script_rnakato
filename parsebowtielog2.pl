66963927f918220859a4744dee1f1e9f255223c3
rnakato <rnakato@iam.u-tokyo.ac.jp>
2016-02-17 14:20:56 +0900

    parsebowtielog2.pl

--- parsebowtielog2.pl
+++ parsebowtielog2.pl
@@ -2,7 +2,7 @@
 
 =head1 DESCRIPTION
  
-    Parse stats output by bowtie2.
+    Parse stats outputted by bowtie2.
  
 =head1 SYNOPSIS
 
@@ -30,17 +30,25 @@ while(<$fh>){
     if($_ =~ /    (.+) (\(.+\)) aligned exactly (.+) time/){
 	$k = $3;
 	last;
+    }elsif($_=~ /    (.+) (\(.+\)) aligned concordantly exactly (.+) time/){
+	$k = $3;
+	last;
     }
 }
+$fh->close;
 
 my $sample="";
 my $num_total="";
 my $num_mapped="";
+my $num_paired="";
 my $num_unaligned="";
 my $num_filtered="";
-my $num_pcrfiltered=" -";
 
-print "Sample\treads\tmapped $k time\t%\tmapped >$k time\t%\tmapped total\t%\tunmapped\t%\n";
+if(!$pair){
+    print "Sample\treads\tmapped $k time\t%\tmapped >$k time\t%\tmapped total\t%\tunmapped\t%\n";
+}else{
+    print "Sample\treads\tpaired\t%\tmapped $k time\t%\tmapped >$k time\t%\tmapped total\t%\tunmapped\t%\n";
+}
 
 $fh = $file->open('r') or die $!;
 while(<$fh>){
@@ -49,35 +57,54 @@ while(<$fh>){
     if($_ =~ /bowtie2 (.+) (.+)\.fastq (.+)/){
 	if($sample ne ""){
 	    my $totalnum = $num_mapped + $num_filtered;
-	    printf "%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\n", $sample, $num_total, $num_mapped, $num_mapped*100/$num_total, $num_filtered, $num_filtered*100/$num_total, $totalnum, $totalnum*100/$num_total, $num_unaligned, $num_unaligned*100/$num_total, $num_pcrfiltered;
+	    if(!$pair){
+		printf "%s\t%d\t", $sample, $num_total;
+	    }else{
+		printf "%s\t%d\t%d\t%.2f\t", $sample, $num_total, $num_paired, $num_paired*100/$num_total;
+	    }
+	    printf "%d\t%.2f\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\n", $num_mapped, $num_mapped*100/$num_total, $num_filtered, $num_filtered*100/$num_total, $totalnum, $totalnum*100/$num_total, $num_unaligned, $num_unaligned*100/$num_total;
 	    $sample="";
 	    $num_total="";
 	    $num_mapped="";
+	    $num_paired="";
 	    $num_unaligned="";
 	    $num_filtered="";
-	    $num_pcrfiltered=" -";
 	}
 	$sample = $2;
     }elsif($_ =~ /(.+) reads; of these:/){
 	$num_total=$1;
-    }elsif($_ =~ /    (.+) (\(.+\)) aligned exactly (.+) time/){
-	$num_mapped=$1;
-	$k = $2;
-    }elsif($_ =~ /    (.+) (\(.+\)) aligned 0 times/){
-	$num_unaligned=$1;
-    }elsif($_ =~ /    (.+) (\(.+\)) aligned (.+) times/){
-	$num_filtered=$1;
- #   }elsif($_ =~ /(.+) overall alignment rate/){
-#	$per_total_map=$1;
-    }elsif($_ =~ /total: pcr bias position: (.+), filter read num: (.+)/){
-	$num_pcrfiltered=$2;
     }elsif($_ =~ /Warning: Could not open read file/){
 	$sample="";
+    }
+    if(!$pair){
+	if($_ =~ /    (.+) (\(.+\)) aligned exactly (.+) time/){
+	    $num_mapped=$1;
+	    $k = $2;
+	}elsif($_ =~ /    (.+) (\(.+\)) aligned 0 times/){
+	    $num_unaligned=$1;
+	}elsif($_ =~ /    (.+) (\(.+\)) aligned (.+) times/){
+	    $num_filtered=$1;
+	}
+    }else{if($_ =~ /  (.+) (\(.+\)) were paired; of these:/){
+	$num_paired=$1;
+	  }elsif($_ =~ /    (.+) (\(.+\)) aligned concordantly exactly (.+) time/){
+	      $num_mapped=$1;
+	      $k = $2;
+	  }elsif($_ =~ /    (.+) (\(.+\)) aligned concordantly 0 times/){
+	      $num_unaligned=$1;
+	  }elsif($_ =~ /    (.+) (\(.+\)) aligned concordantly (.+) times/){
+	      $num_filtered=$1;
+	  }
     }
 }
 $fh->close;
 
 if($num_total ne ""){
     my $totalnum = $num_mapped + $num_filtered;
-	    printf "%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\t%s\n", $sample, $num_total, $num_mapped, $num_mapped*100/$num_total, $num_filtered, $num_filtered*100/$num_total, $totalnum, $totalnum*100/$num_total, $num_unaligned, $num_unaligned*100/$num_total, $num_pcrfiltered;
+    if(!$pair){
+	printf "%s\t%d\t", $sample, $num_total;
+    }else{
+	printf "%s\t%d\t%d\t%.2f\t", $sample, $num_total, $num_paired, $num_paired*100/$num_total;
+    }
+    printf "%d\t%.2f\t%d\t%.2f\t%d\t%.2f\t%d\t%.2f\n", $num_mapped, $num_mapped*100/$num_total, $num_filtered, $num_filtered*100/$num_total, $totalnum, $totalnum*100/$num_total, $num_unaligned, $num_unaligned*100/$num_total;
 }
