6c97bc3f23ddb8b2c7e58f9b8c379a5076f6a8ba
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-03-06 13:50:31 +0100

    cc

--- correlation_coefficient-1file.pl
+++ correlation_coefficient-1file.pl
@@ -3,18 +3,20 @@
 use strict;
 use warnings;
 use autodie;
-$file=$ARGV[0];
-$line1=$ARGV[1];
-$line2=$ARGV[2];
+use Path::Class;
+my $filename=$ARGV[0];
+my $line1=$ARGV[1];
+my $line2=$ARGV[2];
 
 die "correlation_coefficient-1file.pl <file> <line1> <line2>\n" if($#ARGV !=2);
 
-undef @array_a;
-undef @array_b;
+my @array_a = ();
+my @array_b = ();
 
-$n=0;
-open(ListFile, $file) ||die "error: can't open $file\n";
-while(<ListFile>){
+my $n=0;
+my $file = file($filename);
+my $fh = $file->open('r') or die $!;
+while(<$fh>){
     next if($_ eq "\n");
     chomp;
     my @clm = split(/\s/, $_);
@@ -22,16 +24,18 @@ while(<ListFile>){
     $array_b[$n] = $clm[$line2];
     $n++;
 }
-close (ListFile);
+$fh->close;
 
-$cc = &cor(@array_a, @array_b);
+my $cc = &cor(@array_a, @array_b);
 print "$cc\n";
 
 sub cor{
     my $n=@_;
     my $h=$n/2;
-    for($i=0; $i<$h; $i++){ $x[$i]=shift(@_);}
-    for($i=0; $i<$h; $i++){ $y[$i]=shift(@_);}
+    my @x;
+    my @y;
+    for(my $i=0; $i<$h; $i++){ $x[$i]=shift(@_);}
+    for(my $i=0; $i<$h; $i++){ $y[$i]=shift(@_);}
     my $x_sd = sqrt(&var(@x));
     my $y_sd = sqrt(&var(@y));
     my $xy_cov = &cov(@x, @y);
@@ -44,31 +48,33 @@ sub var{
     my $mean=&mean(@_);
     my $n=@_;
     my $sum=0;
-    for($i=0; $i<$n; $i++){
-	$p = $_[$i] - $mean;
+    for(my $i=0; $i<$n; $i++){
+	my $p = $_[$i] - $mean;
 	$sum += $p*$p;
     }
-    $var = $sum/($n-1);
+    my $var = $sum/($n-1);
     return($var);
 }
 
 sub cov{
     my $n=@_;
     my $h=$n/2;
-    for($i=0;$i<$h;$i++){ $x[$i]=shift(@_);}
-    for($k=0;$k<$h;$k++){ $y[$k]=shift(@_);}
+    my @x;
+    my @y;
+    for(my $i=0;$i<$h;$i++){ $x[$i]=shift(@_);}
+    for(my $k=0;$k<$h;$k++){ $y[$k]=shift(@_);}
     my $x_bar=&mean(@x); 
     my $y_bar=&mean(@y); 
     my $sum=0;
-    for($s=0; $s<$h; $s++){ $sum += ($x[$s]-$x_bar)*($y[$s]-$y_bar);}
-    $cov = $sum/($h-1);
+    for(my $s=0; $s<$h; $s++){ $sum += ($x[$s]-$x_bar)*($y[$s]-$y_bar);}
+    my $cov = $sum/($h-1);
     return($cov);
 }
 
 sub mean{
     my $sum=0;
     $n=@_;
-    for($i=0; $i<$n; $i++){ $sum+=$_[$i];}
-    $mean=$sum/$n;
+    for(my $i=0; $i<$n; $i++){ $sum+=$_[$i];}
+    my $mean=$sum/$n;
     return($mean);
 }
