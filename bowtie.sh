94fdcdbf145fed0c2730d4997dea80962e46297a
rnakato <rnakato@example.com>
2016-05-12 17:48:29 +0900

    bowtie

--- bowtie.sh
+++ bowtie.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if test $# -ne 3; then
-    echo "bowtie.sh <prefix> <build> <type>"
+if test $# -ne 4; then
+    echo "bowtie.sh <fastq> <prefix> <build> <type>"
     exit 0
 fi
 
-prefix=$1
-build=$2
-type=$3
+fastq=$1
+prefix=$2
+build=$3
+type=$4
 
 bamdir=bam
 if test ! -e $bamdir; then mkdir $bamdir; fi
@@ -15,23 +16,24 @@ if test ! -e log; then mkdir log; fi
 
 Ddir=/home/Database/UCSC/$build
 
+
 ex_hiseq(){
     index=/home/Database/bowtie-indexes/UCSC-$build
-    command="bowtie -S $index fastq/$prefix.fastq -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"    
+    command="bowtie -S $index $fastq -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"    
     echo $command
     eval $command
 }
 
 ex_csfasta(){
     index=/home/Database/bowtie-indexes/UCSC-$build-cs
-    command="bowtie -S -C $index -f csfasta/$prefix.csfasta -Q csfasta/$prefix.qual -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"
+    command="bowtie -S -C $index -f $fastq.csfasta -Q $fastq.qual -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"
     echo $command
     eval $command
 }
 
 ex_csfastq(){
     index=/home/Database/bowtie-indexes/UCSC-$build-cs
-    command="bowtie -S -C $index fastq/$prefix.fastq -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"
+    command="bowtie -S -C $index $fastq -n2 -m1 --chunkmbs 2048 -p12 | samtools view -bS - | samtools sort - $bamdir/$prefix-n2-m1-$build.sort"
     echo $command
     eval $command
 }
