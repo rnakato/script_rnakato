43f00804f0020946860dc3ddb174252da94d7a9a
rnakato <rnakato@example.com>
2016-05-30 16:42:44 +0900

    edgeR

--- edgeR.R
+++ edgeR.R
@@ -170,11 +170,10 @@ cnts_sig_up <- cnts_sig[sig_up,]
 sig_down <- cnts_sig$logFC < 0
 cnts_sig_down <- cnts_sig[sig_down,]
 
-write.csv(cnts, file=paste(output, ".edgeR.all.xls", sep=""), quote=F)
-write.csv(cnts_sig, file=paste(output, ".edgeR.DEGs.xls", sep=""), quote=F)
-write.csv(cnts_sig_up, file=paste(output, ".edgeR.upDEGs.xls", sep=""), quote=F)
-write.csv(cnts_sig_down, file=paste(output, ".edgeR.downDEGs.xls", sep=""), quote=F)
-
+write.csv(cnts,          file=paste(output, ".edgeR.all.csv",      sep=""), quote=F, sep = "\t")
+write.csv(cnts_sig,      file=paste(output, ".edgeR.DEGs.csv",     sep=""), quote=F, sep = "\t")
+write.csv(cnts_sig_up,   file=paste(output, ".edgeR.upDEGs.csv",   sep=""), quote=F, sep = "\t")
+write.csv(cnts_sig_down, file=paste(output, ".edgeR.downDEGs.csv", sep=""), quote=F, sep = "\t")
 
 # zスコアを用いてクラスタリング
 library(som)
--- edgeR.sh
+++ edgeR.sh
@@ -22,6 +22,11 @@ if test $p = "density"; then
     ex "$R -i=$outname.genes.TPM.$build.txt    -n=$n -o=$outname.genes.TPM.$build    -density"
     ex "$R -i=$outname.isoforms.TPM.$build.txt -n=$n -o=$outname.isoforms.TPM.$build -density -nrowname=2"
 else
-    ex "$R -i=$outname.genes.count.$build.txt    -n=$n -o=$outname.genes.count.$build    -p=$p"
-    ex "$R -i=$outname.isoforms.count.$build.txt -n=$n -o=$outname.isoforms.count.$build -p=$p -nrowname=2 -color=orange"
+ #   ex "$R -i=$outname.genes.count.$build.txt    -n=$n -o=$outname.genes.count.$build    -p=$p"
+  #  ex "$R -i=$outname.isoforms.count.$build.txt -n=$n -o=$outname.isoforms.count.$build -p=$p -nrowname=2 -color=orange"
+    for str in genes isoforms; do
+	s=""
+	for ty in all DEGs upDEGs downDEGs;do s="$s -i $outname.$str.count.$build.edgeR.$ty.csv"; done
+	csv2xlsx.pl $s -o $outname.$str.count.$build.edgeR.xlsx -d,
+    done
 fi
