bc3b46500e08932bdcefef5dbf2427169595738a
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-06-24 17:46:25 +0900

    gts

--- random_pickup2_forfa.pl
+++ random_pickup2_forfa.pl
@@ -5,34 +5,39 @@ use warnings;
 use autodie;
 die "random_pickup2_forfa.pl <file> <number>\n" if($#ARGV !=1);
 
-$file=$ARGV[0];
-$number=$ARGV[1];
+my $file=$ARGV[0];
+my $number=$ARGV[1];
+
+my $wc = `wc -l $file`;
+my @c = split(/ /, $wc);
+my $nread = $c[0]/2;
 
 open(File, $file) ||die "error: can't open $file.\n";
-
-@headarray=(); @faarray=();
-while($line = <File>){
+while(my $line = <File>){
     next if($line eq "\n");
     if($line =~ />/){
-	push (@headarray, $line);
-	$line = <File>;
-	push (@faarray, $line);
+	my $line2 = <File>;
+	if(int(rand($nread)) <= $number) {
+	    print $line;
+	    print $line2;
+	}
     }
 }
 close (File);
 
-for(;;){
-    $x = int(rand()*$#headarray);
-    if($headarray[$x] ne "used"){
-	print $headarray[$x];
-	print $faarray[$x];
-	$headarray[$x] = "used";
-	$num++;
-	if($num>=$number){ exit;}
-    }else{
-	next;
-    }
-}
+#my $num=0;
+#for(;;){
+#    my $x = int(rand()*$nread);
+#    if($headarray[$x] ne "used"){#
+#	print $headarray[$x];
+#	print $faarray[$x];
+#	$headarray[$x] = "used";
+#	$num++;
+#	if($num>=$number){ exit;}
+ #   }else{
+#	next;
+ #   }
+#}
 
 
 
