a122ab81e4a9edff813452896fea9b0067a6319b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-06-28 14:36:36 +0900

    bias

--- make_biasedfa.pl
+++ make_biasedfa.pl
@@ -9,7 +9,13 @@ my $flenfile="";
 my $p="";
 my $bed="";
 
-GetOptions('tablepath|t=s' => \$tablepath, 'chr|c=s' => \$chr, 'csv|v=s' => \$csv, 'flenfile|f=s' => \@flenfile, 'p|p=s' => \$p, 'bed|b=s' => \$bed);
+GetOptions('tablepath|t=s' => \$tablepath, 'chr|c=s' => \$chr, 'csv|v=s' => \$csv, 'flenfile|f=s' => \$flenfile, 'p|p=s' => \$p, 'bed|b=s' => \$bed);
+
+print "$tablepath\n";
+print "$chr\n";
+print "$csv\n";
+print "$flenfile\n";
+print "$p\n";
 
 if($tablepath eq "" || $chr eq ""|| $csv eq ""|| $flenfile eq ""|| $p eq ""){
     print "    make_biasedfa.pl: make random reads for fasta format.\n\n";
@@ -18,18 +24,20 @@ if($tablepath eq "" || $chr eq ""|| $csv eq ""|| $flenfile eq ""|| $p eq ""){
     exit;
 }
 
-my @bedchr;
+#my @bedchr;
 my @bedstart;
 my @bedend;
+my $nbed=0;
 if($bed ne "") {
     open(IN, $bed) || die "error: cannot open $csv.\n";
     while(<IN>) {
 	next if($_ eq "\n" || $_ =~ /#/);
 	chomp;
 	@clm= split(/\t/, $_);
-	push(@bedchr, $clm[0]);
+#	push(@bedchr, $clm[0]);
 	push(@bedstart, $clm[1]);
 	push(@bedend, $clm[2]);
+	$nbed++;
     }
     close IN;
 }
@@ -97,7 +105,15 @@ for($i=0; $i<$len-5-300; $i++){
     my $gc = substr($flag, 5, $flen4gc);
     $gc = $gc =~ s/[GC]//ig;
     $gc = 0 if($gc eq "");
-    if(rand($max) < $GCarray[$gc]*$p){
+    my $pran = rand($max);
+    my $on=0;
+    $on = 1 if($pran < $GCarray[$gc]*$p);
+    for(my $j=0; $j<$nbed; $j++) {
+	$on = 1 if($bedstart[$j]< $i && $i < $bedend[$j] && $pran < $GCarray[$gc]*$p*10); # 10 fold in peak region
+	last if($bedstart[$j] > $i);
+    }
+    
+    if($on){
 	if(rand(1) < 0.5) {
 	    my $read = substr($fasta, $i, $readlen);
 	    print ">chr${chr}_$i:+\n$read\n";
