af84e9327cff687530ec04258b100eb1c176e877
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-07-18 20:30:04 +0900

    test

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-
 import sys
 import matplotlib
 #matplotlib.use('Agg')
@@ -16,6 +15,7 @@ status = []
 num = 0
 hash = {}
 
+file=open("/home/rnakato/temp/2015_017A_KT55_D-n2-m1-hg38-raw-mpbl-sm0.comp1.bed", 'r')
 file=open(sys.argv[1], 'r')
 for line in file:
     if '#' in line or 'chromosome' in line:
@@ -25,31 +25,34 @@ for line in file:
     if clm[3] == 'intergenic':
         continue
     status.append(clm[3])
-
     if not hash.has_key(clm[3]):
         hash.update({clm[3] :num})
         num += 1
+
 file.close()
 
 #print hash
 #print len(hash)
 #print len(status)
 
+window = 100
 data = [[0 for i in range(len(hash))] for j in range(len(status))]
 for i in range(0, len(status)):
 #    print status[i]
  #   print hash[status[i]]
-    data[i][hash[status[i]]] = 1
+    data[i/window][hash[status[i]]] += 1
 
 label = []
 for st in hash:
 #    print hash[st], st
     label.insert(hash[st], st)
 
-fig = matplotlib.pyplot.figure()
-ax = fig.add_subplot(1,1,1)
+#fig = matplotlib.pyplot.figure()
+#ax = fig.add_subplot(1,1,1)
 ax = sns.heatmap(data, cmap='Blues')
 labels = ax.set_xticklabels(label,fontsize ="small")
 sns.plt.axis()
-#sns.plt.show()
+sns.plt.show()
 fig.savefig(sys.argv[1] + ".heatmap.png")
+
+#fig.savefig("heatmap.png")
