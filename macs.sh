7c4b29f9f8ef9887d2c45f50e117fbae0d16ada8
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-07-29 18:29:07 +0900

    no Input

--- macs.sh
+++ macs.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 function usage()
 {
-    echo "macs.sh [-s fraglen/2] [-q qvalue] <IP bam> <Input bam> <output> [sharp|broad|nomodel|broad-nomodel]" 1>&2
+    echo "macs.sh [-p species] [-s fraglen/2] [-q qvalue] <IP bam> <Input bam> <output> [sharp|broad|nomodel|broad-nomodel]" 1>&2
 }
 
 flen=100
 qval=0.05
-while getopts s:q: option
+species="hs"
+while getopts p:s:q: option
 do
     case ${option} in
+        p)
+            species=${OPTARG}
+            ;;
 	s)
 	    flen=${OPTARG}
 	    ;;
@@ -36,21 +40,28 @@ Input=$2
 peak=$3
 mode=$4
 
-macs="macs2 callpeak -t $IP -c $Input -g hs -f BAM"
 if test -e $IP && test -s $IP ; then
-    if test -e $Input && test -s $Inpput ; then
-	if test $mode = "nomodel"; then
-	    if test ! -e $mdir/${peak}-nomodel_summits.bed; then $macs -q $qval -n $mdir/$peak-nomodel --nomodel --shift $flen; fi
-	elif test $mode = "broad"; then
-	    if test ! -e $mdir/${peak}_summits.bed; then         $macs -q $qval -n $mdir/$peak --broad; fi
-	elif test $mode = "broad-nomodel"; then
-	    if test ! -e $mdir/${peak}-nomodel_summits.bed; then $macs -q $qval -n $mdir/$peak-nomodel --nomodel --shift $flen --broad; fi
-	else 
-	    if test ! -e $mdir/${peak}_summits.bed; then         $macs -q $qval -n $mdir/$peak; fi
-	fi
-    else
-	echo "$Input does not exist."
-    fi
+    n=1
 else
     echo "$IP does not exist."
+fi
+if test $Input = "none"; then
+    macs="macs2 callpeak -t $IP -g $species -f BAM"
+else
+    macs="macs2 callpeak -t $IP -c $Input -g $species -f BAM"
+    if test -e $Input && test -s $Input; then
+	n=1
+    else
+        echo "$Input does not exist."
+    fi
+fi
+
+if test $mode = "nomodel"; then
+    if test ! -e $mdir/${peak}-nomodel_summits.bed; then $macs -q $qval -n $mdir/$peak-nomodel --nomodel --shift $flen; fi
+elif test $mode = "broad"; then
+    if test ! -e $mdir/${peak}_summits.bed; then         $macs -q $qval -n $mdir/$peak --broad; fi
+elif test $mode = "broad-nomodel"; then
+    if test ! -e $mdir/${peak}-nomodel_summits.bed; then $macs -q $qval -n $mdir/$peak-nomodel --nomodel --shift $flen --broad; fi
+else 
+    if test ! -e $mdir/${peak}_summits.bed; then         $macs -q $qval -n $mdir/$peak; fi
 fi
