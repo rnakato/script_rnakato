faf09263da56d2d22fcc503002f5020671fd096e
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-10-03 19:04:11 +0900

    test

--- addranking4multiBamCov.pl
+++ addranking4multiBamCov.pl
@@ -38,14 +38,17 @@ while(<$fh>){
 	$ncol = $#clm +1;
 	$nsample = $ncol - 6;
 #	print "$ncol $nsample\n";
+	for(my $i=0; $i<$ncol; $i++) { print "$clm[$i]\t";}
+	for(my $i=6; $i<$ncol; $i++) { print "$clm[$i]\t";}
+	print "\n";
 	for(my $i=0; $i<$ncol+$nsample; $i++) {
 	    my @a;
 	    push @array, [@a];
 	}
-    }
-
-    for(my $i=0; $i<=$#clm; $i++) {
-	push(@{$array[$i]}, $clm[$i]);
+    } else{
+	for(my $i=0; $i<=$#clm; $i++) {
+	    push(@{$array[$i]}, $clm[$i]);
+	}
     }
     $nline++;
 }
@@ -55,7 +58,7 @@ for(my $i=0; $i<$nsample; $i++) {
     @{$array[$i+$ncol]} = sort { $b <=> $a } @{$array[$i+6]};
 }
 
-for(my $j=0; $j<$nline; $j++) {
+for(my $j=0; $j<$nline-1; $j++) {
     for(my $i=0; $i<$ncol; $i++) {
 	print "$array[$i][$j]\t";
     }
