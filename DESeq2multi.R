656b571f66f6f4f1bac8f81a90618a20135c4ed1
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2016-12-19 19:27:11 +0900

    test

---
+++ DESeq2multi.R
@@ -0,0 +1,233 @@
+print.usage <- function() {
+	cat('\nUsage: Rscript edgeR.R <options>\n',file=stderr())
+	cat('   MANDATORY ARGUMENTS\n',file=stderr())
+	cat('      -i=<input file>  , input file (RSEM gene/transcript file, estimated count) \n',file=stderr())
+	cat('      -n=<num1>:<num2>:<num3>, num of replicates for each group \n',file=stderr())
+	cat('   OPTIONAL ARGUMENTS\n',file=stderr())
+	cat('      -nrowname=<int> , row name (default: 1) \n',file=stderr())
+	cat('      -gname=<name1>:<name2>:<num3> , name of each group \n',file=stderr())
+	cat('      -p=<float>      , threshold for FDR (default: 0.01) \n',file=stderr())
+	cat('   OUTPUT ARGUMENTS\n',file=stderr())
+	cat('      -o=<output> , prefix of output file \n',file=stderr())
+	cat('\n',file=stderr())
+}
+
+args <- commandArgs(trailingOnly = T) 
+nargs = length(args);
+minargs = 1;
+maxargs = 7;
+if (nargs < minargs | nargs > maxargs) {
+	print.usage()
+	q(save="no",status=1)
+}
+
+gname1 <- "groupA"
+gname2 <- "groupB"
+gname3 <- "groupC"
+p <- 0.01
+nrowname <- 1
+
+for (each.arg in args) {
+    if (grepl('^-i=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) {
+            filename <- arg.split[2]
+        }
+        else { stop('No input file name provided for parameter -i=')}
+    }
+    else if (grepl('^-n=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) {
+            sep.vals <- arg.split[2]
+            sep.vals.split <- strsplit(sep.vals,':',fixed=TRUE)[[1]]     
+            if (length(sep.vals.split) != 3) {
+                stop('must be specified as -n=<num1>:<num2>:<num3>')                    
+            } else {
+                if (any(is.na(as.numeric(sep.vals.split)))) { # check that sep vals are numeric
+                    stop('must be numeric values')
+                }
+                num1 <- as.numeric(sep.vals.split[1])
+                num2 <- as.numeric(sep.vals.split[2])
+ 		num3 <- as.numeric(sep.vals.split[3])
+            }      				
+        }
+        else { stop('No value provided for parameter -n=')}
+    }
+    else if (grepl('^-gname=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) {
+            sep.vals <- arg.split[2]
+            sep.vals.split <- strsplit(sep.vals,':',fixed=TRUE)[[1]]     
+            if (length(sep.vals.split) != 3) {
+                stop('must be specified as -gname=<gname1>:<gname2>:<gname3>')                    
+            } else {
+                gname1 <- sep.vals.split[1]
+                gname2 <- sep.vals.split[2]
+                gname3 <- sep.vals.split[3]
+            }      
+        }
+        else { stop('No value provided for parameter -gname=')}
+    }
+    else if (grepl('^-nrowname=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) {
+            nrowname <- as.numeric(arg.split[2])
+        }
+        else { stop('No value provided for parameter -nrowname=')}
+    }
+    else if (grepl('^-p=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) {
+            p <- as.numeric(arg.split[2])
+        }
+        else { stop('No value provided for parameter -p=')}
+    }
+    else if (grepl('^-o=',each.arg)) {
+        arg.split <- strsplit(each.arg,'=',fixed=TRUE)[[1]]
+        if (! is.na(arg.split[2]) ) { output <- arg.split[2] }
+        else { stop('No output file name provided for parameter -o=')}
+    }
+}
+
+filename
+nrowname
+p
+#num1
+#num2
+output
+
+group <- data.frame(group = factor(c(rep(gname1,num1),rep(gname2,num2),rep(gname3,num3))))
+
+# filename <- "Matrix.isoforms.count.GRCh38.txt"
+# group <- data.frame(group = factor(c(rep("Normal",4),rep("CdLS",2),rep("CHOPS",4))))
+# data <- read.table(filename, header=T, row.names=2, sep="\t")
+# data <- data[,-1]
+
+### read data
+cat('\nread in', filename, '\n',file=stdout())
+if(nrowname==2){
+    data <- read.table(filename, header=T, row.names=nrowname, sep="\t")
+    data <- data[,-1]
+}else{
+    data <- read.table(filename, header=T, row.names=nrowname, sep="\t")
+}
+counts <- as.matrix(data)
+counts <- floor(counts) # DESeq2は整数しか受け付けない
+counts <- subset(counts,rowSums(counts)>1) # 値が1より大きいものだけ残す
+
+library(DESeq2)
+library(ggplot2)
+# データ読み込みとDEGs計算
+dds <- DESeqDataSetFromMatrix(countData = counts, colData = group, design = ~ group)
+dds <- DESeq(dds)
+# 結果の抽出
+res.A.B <- results(dds, contrast=c("group",gname1,gname2), alpha = p)
+res.A.C <- results(dds, contrast=c("group",gname1,gname3), alpha = p)
+head(res.A.B)
+head(res.A.C)
+# 結果表示
+summary(res.A.B)
+summary(res.A.C)
+pdf(paste(output, ".DESeq2.MAplot.pdf", sep=""), height=7, width=7)
+plotMA(res.A.B, main=paste(gname1, gname2, sep="-"), ylim=c(-2,2), alpha = p)
+plotMA(res.A.C, main=paste(gname1, gname3, sep="-"), ylim=c(-2,2), alpha = p)
+dev.off()
+
+## ----log+1よりも頑健な補正法
+#rld <- rlog(dds)
+vsd <- varianceStabilizingTransformation(dds)  #　これを推奨
+#vsd.fast <- vst(dds)                          #　vsdをサンプル抽出でこなす
+
+#rlogMat <- assay(rld)
+vsdMat <- assay(vsd)
+#vsdfastMat <- assay(vsd.fast)
+
+
+# FDRでランキング
+resAndvsd <- transform(exp=assay(vsd), res)
+resOrdered <- resAndvsd[order(res$padj),]
+
+pdf(paste(output, ".topDEGs.pdf", sep=""), height=14, width=14)
+par(mfrow=c(3,3))
+topDEGsid <- order(res$padj, decreasing=F)[1:9]
+for(i in topDEGsid) {
+    plotCounts(dds, gene=i, intgroup="group")
+}
+dev.off()
+
+## SD against mean
+library("vsn")
+pdf(paste(output, ".MeanVariance.pdf", sep=""), height=7, width=9)
+par(mfrow=c(1,2))
+meanSdPlot(log2(counts(dds,normalized=T) + 1))
+#meanSdPlot(rlogMat)
+meanSdPlot(vsdMat)
+#meanSdPlot(vsdfastMat)
+dev.off()
+
+# heatmap of top20 highly-expressed genes
+library(pheatmap)
+select <- order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:20]
+
+nt <- normTransform(dds) # log2(x+1)
+df <- as.data.frame(colData(dds)[,c("group","group")])
+pdf(paste(output, ".HighlyExpressedGenes.pdf", sep=""), height=7, width=7)
+#par(mfrow=c(1,2))
+pheatmap(assay(nt)[select,], cluster_rows=F, show_rownames=F, cluster_cols=F, annotation_col=df)
+pheatmap(vsdMat[select,], cluster_rows=F, show_rownames=F, cluster_cols=F, annotation_col=df)
+dev.off()
+
+# sample clustering
+library("RColorBrewer")
+sampleDists <- dist(t(vsdMat))
+sampleDistMatrix <- as.matrix(sampleDists)
+#rownames(sampleDistMatrix) <- paste(rld$condition, rld$type, sep="-")
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pdf(paste(output, ".sampleClustering.pdf", sep=""), height=7, width=8)
+pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists, col=colors)
+dev.off()
+
+# PCA plot
+pdf(paste(output, ".samplePCA.pdf", sep=""), height=7, width=7)
+plotPCA(vsd, intgroup=c("group"))
+dev.off()
+
+q("no")
+
+
+# multifactor designs
+#designの中の特定のtypeをcontrastにする（single-factor Wald test）
+res.A.B <- results(dds, contrast=c("condition","A","B"))
+res.A.C <- results(dds, contrast=c("condition","A","C"))
+res.B.C <- results(dds, contrast=c("condition","B","C"))
+
+head()
+resOrdered <- res.A.B[order(res.A.B$padj),]
+
+# one-way ANOVA (p-value indicates difference at least in one condition)
+ddsLRT <- DESeq(dds, test="LRT", reduced= ~ 1)
+resLRT <- results(ddsLRT)
+
+
+# 多群間二因子比較
+group <- data.frame(
+  condition = factor(c(rep("K",6),rep("W",6))),
+  day = factor(c(rep(c(0,2,7),4)))
+)
+model.matrix(~ group$con + group$day)
+
+dds <- nbinomLRT(dds, full = ~ condition + day, reduced = ~ day)
+res <- results(dds)
+res
+head(res[order(res$pvalue), ])
+
+dds <- nbinomLRT(dds, full = ~ condition + day, reduced = ~ condition)
+res <- results(dds)
+res
+head(res[order(res$pvalue), ])
+
+dds <- estimateSizeFactors(dds)
+dds <- estimateDispersions(dds)
+res <- results(dds)
+
