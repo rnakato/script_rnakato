fe9f150c546de315fad2b260d505062bf7aa1d9b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-10-20 16:44:39 +0900

    add addChain4pastis.pl

--- addChain4pastis.pl
+++ addChain4pastis.pl
@@ -3,10 +3,12 @@ use strict;
 use warnings;
 
 my $filename=$ARGV[0];
+my $output=$filename . ".addCONECT.pdb";
 
 my $num = `wc -l $filename | cut -f1 -d' '`;
+system("cp $filename $output");
 
-open(FILE, ">>", $filename) or die;
+open(FILE, ">>", $output) or die;
 for (my $i=0;$i<$num-1;$i++) {
     printf FILE "CONECT%5d%5d\n", $i, $i+1;
 }
