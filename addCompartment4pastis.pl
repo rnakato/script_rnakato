8ef2e99486becacd8e98386c4765a9760d4f2f5c
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-10-20 18:41:38 +0900

    add addCompartment4pastis.pl

---
+++ addCompartment4pastis.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $filename=$ARGV[0];
+my $bedgraph=$ARGV[1];
+my $output=$filename . ".addCompartment.pdb";
+
+open(File, $bedgraph) ||die "error: can't open $bedgraph\n";
+my @compartment = ();
+while(<File>) {
+    next if($_ eq "\n");
+    chomp;
+    my @clm = split(/\t/, $_);
+    if($clm[3]>=0) {
+	push(@compartment, "A");
+    } else {
+	push(@compartment, "B");
+    }
+}
+close (File);
+
+#for(my $i=0;$i<=$#compartment;$i++){
+#    print "$compartment[$i]\n";
+#}
+
+open(FILE, ">", $output) or die;
+open(PDB, $filename) ||die "error: can't open $filename\n";
+my $num = 0;
+while(<PDB>) {
+    next if($_ eq "\n");
+    if($compartment[$num] eq "B") {
+	$_ =~ s/A       /B       /g;
+    }
+    printf FILE "$_";
+    $num++;
+}
+close(FILE);
+close(PDB);
