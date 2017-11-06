6a12c7bad6c8237def911b9cd68aa6cc9c93136b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-11-06 18:25:22 +0900

    test

--- matrix2npy.py
+++ matrix2npy.py
@@ -7,4 +7,16 @@ inputfile = sys.argv[1]
 outputfile = sys.argv[2]
 
 matrix = np.loadtxt(inputfile,delimiter="\t")
-np.save(outputfile, matrix)
+
+if len(sys.argv) > 6:
+    s1 = int(sys.argv[3])
+    e1 = int(sys.argv[4])
+    s2 = int(sys.argv[5])
+    e2 = int(sys.argv[6])
+    matrix = matrix[s1:e1,s2:e2]
+elif len(sys.argv) > 4:
+    s = int(sys.argv[3])
+    e = int(sys.argv[4])
+    matrix = matrix[s:e,s:e]
+
+np.savetxt(outputfile, matrix)
