d0d7845c9dbba10ed8072895d0de23936eef2c0e
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-11-07 16:06:45 +0900

    test

--- addChain4pastis.pl
+++ addChain4pastis.pl
@@ -5,11 +5,34 @@ use warnings;
 my $filename=$ARGV[0];
 my $output=$filename . ".addCONECT.pdb";
 
-my $num = `wc -l $filename | cut -f1 -d' '`;
-system("cp $filename $output");
+open(PDB, $filename) || die "error: can't open $filename\n";
+my @array=();
+my @linearray=();
+while(<PDB>) {
+    next if($_ eq "\n" || $_ =~ /nan/);
+    push (@linearray, $_);
+    
+    chomp;
+    my @clm = split(/\s+/, $_);
+    push (@array, $clm[1]);
+#    print "$clm[1]\n";
+#    print "$clm[5]\t$clm[6]\t$clm[7]\n";
+}
+close(PDB);
 
-open(FILE, ">>", $output) or die;
-for (my $i=0;$i<$num-1;$i++) {
-    printf FILE "CONECT%5d%5d\n", $i, $i+1;
+#my $num = `wc -l $filename | cut -f1 -d' '`;
+#system("cp $filename $output");
+
+open(FILE, ">", $output) or die;
+foreach my $line (@linearray){
+  print FILE $line;
+}
+
+my $num=-100;
+foreach my $id (@array){
+    if($id-$num==1) {
+	printf FILE "CONECT%5d%5d\n", $num, $id;
+    }
+    $num = $id;
 }
 close(FILE)
