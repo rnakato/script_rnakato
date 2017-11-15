1a5f9652e4744d38545357d72a78849e0f49206a
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-11-15 16:47:50 +0900

    test

--- getOverlappedfromMultiBed.sh
+++ getOverlappedfromMultiBed.sh
@@ -5,10 +5,10 @@ nbed=$#
 tmpfile1=$(mktemp)
 tmpfile2=$(mktemp)
 
-cp $1 $tmpfile1
+cat $1 | grep -v \# | grep -v chromosome | sort -k1,1 -k2,2n > $tmpfile1
 for bed in $beds
 do
-    bedtools intersect -a $tmpfile1 -b $bed > $tmpfile2
-    cp $tmpfile2 $tmpfile1
+  cat $bed | grep -v \# | grep -v chromosome | sort -k1,1 -k2,2n | bedtools intersect -a $tmpfile1 -b - > $tmpfile2
+  cp $tmpfile2 $tmpfile1
 done
 cat $tmpfile2
