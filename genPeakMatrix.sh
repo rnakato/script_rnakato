a1db8e3b362f8c89a4627ef44e2e6f35b4f1afb3
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-11-22 19:44:33 +0900

    test

--- genPeakMatrix.sh
+++ genPeakMatrix.sh
@@ -19,12 +19,17 @@ tmpfile=$(mktemp)
 cut -f1,2,3 $bed | grep -v \# | grep -v chromosome > $tmpfile
 
 tmpfile2=$(mktemp)
-tmpfile3=$(mktemp)
 cp $tmpfile $tmpfile2
 
+tmpfile3=$(mktemp)
 for bam in $bams; do
     bedtools coverage -a $tmpfile -b $bam -c | cut -f1,4 | join -t $'\t' $tmpfile2 - > $tmpfile3
     cp $tmpfile3 $tmpfile2
 done
 
+
+for bam in $bams; do
+    echo -en "\t$bam" 
+done
+echo "" 
 cat $tmpfile2
