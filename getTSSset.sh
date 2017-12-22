039851c9ae81f19a3f70db0ec60d9ffbb5148464
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2017-12-22 13:23:09 +0900

    test

--- getTSSset.sh
+++ getTSSset.sh
@@ -11,8 +11,12 @@ cat $refFlat | awk '{if($13=="protein_coding") print;}' > $pcgene
 cut -f3,5,6 $pcgene | addchr.pl - > $chrtemp
 
 temp=$(mktemp)
-bedtools multicov -bams $bams -bed $chrtemp | paste $pcgene - > $temp
+echo "bedtools multicov -bams $bams -bed $chrtemp > $temp.temp"
+bedtools multicov -bams $bams -bed $chrtemp > $temp.temp
+if test ! -s $temp; then exit; fi
+paste $pcgene $temp.temp > $temp
+
 getMaxvalTSS.pl -n $nsample $temp > $output.all.csv
 cut -f1,2,3,4,5,6,7,8,9,10,11,12,13 $output.all.csv > $output.refFlat
 
-rm $pcgene $chrtemp $temp
+rm $pcgene $chrtemp $temp.temp $temp
