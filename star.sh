683c6645f943ce7ceb1229f7278580b3fbd41db6
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2018-01-16 13:34:21 +0900

    test

--- star.sh
+++ star.sh
@@ -34,6 +34,9 @@ mkdir -p log $odir
 if test $build = "S_pombe" -o $build = "S_cerevisiae"; then
     index_star=`database.sh`/rsem-star-indexes/$build
     index_rsem=`database.sh`/rsem-star-indexes/$build/$build
+elif test $build = "GRCh37.proteincoding"; then
+    index_star=`database.sh`/rsem-star-indexes/$db-$build
+    index_rsem=`database.sh`/rsem-star-indexes/$db-$build/$db-GRCh37
 else
     index_star=`database.sh`/rsem-star-indexes/$db-$build
     index_rsem=`database.sh`/rsem-star-indexes/$db-$build/$db-$build
@@ -53,17 +56,14 @@ fi
 
 STARdir=$(cd $(dirname $0) && pwd)/../STAR/bin/Linux_x86_64_static
 
-$STARdir/STAR --genomeLoad NoSharedMemory --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM \
-    --runThreadN 12 --outSAMattributes All $pzip \
-    --genomeDir $index_star --readFilesIn $fastq $parstr \
-    --outFileNamePrefix $odir/$prefix.$build.
+#$STARdir/STAR --genomeLoad NoSharedMemory --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM \
+#    --runThreadN 12 --outSAMattributes All $pzip \
+#    --genomeDir $index_star --readFilesIn $fastq $parstr \
+#    --outFileNamePrefix $odir/$prefix.$build.
 
 log=log/star-$prefix.$build.txt
 echo -en "$prefix\t" > $log
 parse_starlog.pl $odir/$prefix.$build.Log.final.out >> $log
-#wigdir=bedGraph
-#if test ! -e $odir/$wigdir; then mkdir $odir/$wigdir; fi
-#$STARdir/STAR --runMode inputAlignmentsFromBAM --runThreadN 12 --inputBAMfile $odir/${prefix}.$build.Aligned.sortedByCoord.out.bam --outWigType bedGraph $parWig --outFileNamePrefix $odir/$wigdir/$prefix.$build --outWigReferencesPrefix chr
 
 RSEMdir=$(cd $(dirname $0) && pwd)/../RSEM
 $RSEMdir/rsem-calculate-expression $pair --alignments --estimate-rspd --forward-prob $prob --no-bam-output -p 12 $odir/${prefix}.$build.Aligned.toTranscriptome.out.bam $index_rsem $odir/$prefix.$build #--calc-ci --ci-memory 30000 --seed 12345
