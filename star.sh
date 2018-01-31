ea232441e6433075538eb7ec5420dce1c52d1ea3
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2018-01-31 11:48:35 +0900

    test

--- star.sh
+++ star.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cmdname=`basename $0`
+function usage()
+{
+    echo "$scriptname [-d outputdir] <single|paired> <output prefix> <fastq> <Ensembl|UCSC> <build> <--forward-prob [0-1]>" 1>&2
+}
 
 odir=star
 while getopts d: option
@@ -15,11 +20,11 @@ do
 done
 shift $((OPTIND - 1))
 
-scriptname=${0##*/}
-if test $# -ne 6; then
-    echo "$scriptname [-d outputdir] <single|paired> <output prefix> <fastq> <Ensembl|UCSC> <build> <--forward-prob [0-1]>"
-    exit 0
+if [ $# -ne 6 ]; then
+  usage
+  exit 1
 fi
+
 
 readtype=$1
 prefix=$2
@@ -38,7 +43,12 @@ else
     index_rsem=`database.sh`/rsem-star-indexes/$db-$build/$db-$build
 fi
 
-if test $readtype = "paired"; then pair="--paired-end"; fi
+if test $readtype = "paired"; then pair="--paired-end"
+elif ! test $readtype = "single"; then
+    echo "Error: specify [single|paired]"
+  usage
+  exit 1
+fi
 if test $prob = "0.5"; then  # unstraned
     parstr="--outSAMstrandField intronMotif"
     parWig="--outWigStrand Unstranded"
@@ -52,10 +62,10 @@ fi
 
 STARdir=$(cd $(dirname $0) && pwd)/../STAR/bin/Linux_x86_64_static
 
-$STARdir/STAR --genomeLoad NoSharedMemory --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM \
-    --runThreadN 12 --outSAMattributes All $pzip \
-    --genomeDir $index_star --readFilesIn $fastq $parstr \
-    --outFileNamePrefix $odir/$prefix.$build.
+#$STARdir/STAR --genomeLoad NoSharedMemory --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM \
+#    --runThreadN 12 --outSAMattributes All $pzip \
+#    --genomeDir $index_star --readFilesIn $fastq $parstr \
+#    --outFileNamePrefix $odir/$prefix.$build.
 
 log=log/star-$prefix.$build.txt
 echo -en "$prefix\t" > $log
@@ -65,4 +75,4 @@ RSEMdir=$(cd $(dirname $0) && pwd)/../RSEM
 $RSEMdir/rsem-calculate-expression $pair --alignments --estimate-rspd --forward-prob $prob --no-bam-output -p 12 $odir/${prefix}.$build.Aligned.toTranscriptome.out.bam $index_rsem $odir/$prefix.$build #--calc-ci --ci-memory 30000 --seed 12345
 
 #$RSEMdir/rsem-plot-transcript-wiggles --gene-list --show-unique mmliver_single_quals gene_ids.txt output.pdf 
-$RSEMdir/rsem-plot-model $odir/$prefix.$build $odir/$prefix.$build.quals.pdf
+#$RSEMdir/rsem-plot-model $odir/$prefix.$build $odir/$prefix.$build.quals.pdf
