ce31a8045efa12ffc313ab09cb1549c53322f4fe
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2018-02-05 10:11:21 +0900

    jaccard

--- jaccard.pl
+++ jaccard.pl
@@ -6,10 +6,10 @@
 
 =head1 SYNOPSIS
 
-    % jaccard.pl [options] -f <filename> -n <name> [-f <filename> -n <name> ...]
+    % jaccard.pl [options] -f <filename> -l <label> [-f <filename> -l <label> ...]
 
     Options:
-    --prop
+    --number  peak number to be compared (default: 10000)
 
 =cut
 
@@ -18,35 +18,34 @@ use warnings;
 use autodie;
 use Getopt::Long qw/:config posix_default no_ignore_case bundling auto_help/;
 use Pod::Usage qw/pod2usage/;
+use File::Temp qw/tempfile tempdir/;
 
 my @file;
 my @name;
-my $pair=0;
+my $number=10000;
 GetOptions(
     "file|f=s@" => \@file,
-    "name|n=s@" => \@name,
-    "prop|p" => \$pair
+    "label|l=s@" => \@name,
+    "number|n=i" => \$number
 ) or pod2usage(1);
 
-die "different num: -f and -n.\n" if($#file != $#name);
+die "different number: -f and -l.\n" if($#file != $#name);
 
-foreach (@name){
-    print "\t$_";
-}
+pod2usage(2) if(!$#file);
+my ($fh, $tmpfile) = tempfile;
+
+for(my $i=0;$i<=$#file;$i++){ system("sort -k1,1 -k2,2n $file[$i] | head -n $number > $tmpfile.$i"); }
+
+foreach (@name){ print "\t$_"; }
 print "\n";
 
 for(my $i=0;$i<=$#file;$i++){
     my $f1 = $file[$i];
     print "$name[$i]";
-    foreach my $f2 (@file){
-	system("sort -k1,1 -k2,2n $f1 > $f1.sorttemp");
-	system("sort -k1,1 -k2,2n $f2 > $f2.sorttemp");
-	my $jaccard=`bedtools jaccard -a $f1.sorttemp -b $f2.sorttemp | grep -v jaccard | cut -f3 | tr -d '\n'`;
+    for(my $j=0;$j<=$#file;$j++){
+	my $f2 = $file[$j];
+	my $jaccard=`bedtools jaccard -a $tmpfile.$i -b $tmpfile.$j | grep -v jaccard | cut -f3 | tr -d '\n'`;
 	print "\t$jaccard";
     }
     print "\n";
-}
-
-foreach my $f2 (@file){
-    unlink "$f2.sorttemp";
 }
