ecdf41b13ff0363df2b502671d52d7b4664cd005
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2018-07-05 15:21:12 +0900

    add Hi-C dir

---
+++ Hi-C/makeMatrix_inter.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+cmdname=`basename $0`
+function usage()
+{
+    echo "$cmdname <matrixdir> <hic file> <binsize> <build> <lim_pzero>" 1>&2
+}
+
+all=0
+while getopts a option
+do
+    case ${option} in
+        a)
+            all=1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [ $# -ne 4 ]; then
+  usage
+  exit 1
+fi
+
+matrixdir=$1
+hic=$2
+binsize=$3
+build=$4
+lim_pzero=$5
+
+gt=/home/Database/UCSC/$build/genome_table
+
+if test $build = "mm10" -o $build = "mm9"; then
+    chrnum=19
+else
+    chrnum=22
+fi
+
+juicertool="java -Xms512m -Xmx2048m -jar /home/git/binaries/Aidenlab/juicer_tools.1.8.9_jcuda.0.8.jar"
+dir=$matrixdir/intrachromosomal/$binsize
+mkdir -p $dir
+
+pwd=$(cd $(dirname $0) && pwd)
+
+for i in $(seq 1 $chrnum); do
+    for j in $(seq $i $chrnum); do
+        d=$dir/interchromosomal/$binsize/chr$i-chr$j
+        mkdir -p $d
+        for type in observed oe; do
+            echo $i $j $type
+            $juicertool dump $type VC_SQRT $hic $i $j BP $binsize $d/$type.matrix -d
+	    gzip -f $d/$type.matrix
+        done
+    done
+done
+
+for str in observed #oe                                                                                                                                                                       
+do
+    $pwd/merge_JuicerMatrix_to_Genome.py $dir/interchromosomal \
+        $dir/interchromosomal/$binsize/genome.$str.full.$lim_pzero.pickle \
+        $binsize $str $lim_pzero
+    $pwd/merge_JuicerMatrix_to_Genome.py $dir/interchromosomal \
+        $dir/interchromosomal/$binsize/genome.$str.evenodd.$lim_pzero.pickle \
+        $binsize $str $lim_pzero --evenodd
+done
---
+++ Hi-C/makeMatrix_intra.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+cmdname=`basename $0`
+function usage()
+{
+    echo "$cmdname <matrixdir> <hic file> <binsize> <build>" 1>&2
+}
+
+all=0
+while getopts a option
+do
+    case ${option} in
+        a)
+            all=1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+if [ $# -ne 4 ]; then
+  usage
+  exit 1
+fi
+
+matrixdir=$1
+hic=$2
+binsize=$3
+build=$4
+
+gt=/home/Database/UCSC/$build/genome_table
+
+if test $build = "mm10" -o $build = "mm9"; then
+    chrnum=19
+else
+    chrnum=22
+fi
+
+juicertool="java -Xms512m -Xmx2048m -jar /home/git/binaries/Aidenlab/juicer_tools.1.8.9_jcuda.0.8.jar"
+dir=$matrixdir/intrachromosomal/$binsize
+mkdir -p $dir
+
+pwd=$(cd $(dirname $0) && pwd)
+
+for chr in $(seq 1 $chrnum) X; do
+    for norm in VC_SQRT #NONE
+    do
+        echo $chr
+        for type in observed oe
+        do
+            $juicertool dump $type $norm $hic $chr $chr BP $binsize $dir/$type.$norm.chr$chr.txt
+            $pwd/convert_JuicerDump_to_dense.py $dir/$type.$norm.chr$chr.txt $dir/$type.$norm.chr$chr.matrix.gz $gt chr$chr $binsize
+            rm $dir/$type.$norm.chr$chr.txt
+        done
+        for type in #expected norm
+        do
+            $juicertool dump $type #$norm $hic.hic $chr BP $binsize $dir/$type.$norm.chr$chr.matrix -d
+        done
+    done
+done
+
+
