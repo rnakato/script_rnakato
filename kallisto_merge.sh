96e449de7ab46c45b26cbbd81942594ef11fcfe6
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2018-07-20 11:53:33 +0900

    modify kallisto

--- kallisto_merge.sh
+++ kallisto_merge.sh
@@ -16,7 +16,7 @@ gtf=$2
 files=${@:3}
 
 pwd=`pwd.sh`
-#$pwd/mergekallistotsv.sh $files > $output.transcript.csv
+$pwd/mergekallistotsv.sh $files > $output.transcript.csv
 $pwd/convert_genename_fromgtf.pl transcript $output.transcript.csv $gtf 0 > $output.transcript.name.csv
 
 Rscript $pwd/kallisto_tximport.R $output.gene $gtf $files
@@ -30,7 +30,7 @@ echo "" >> $tmpfile
 cat $output.gene.csv >> $tmpfile
 mv $tmpfile $output.gene.csv
 
-#$pwd/convert_genename_fromgtf.pl genes $output.gene.csv $gtf 0 > $output.gene.name.csv
+$pwd/convert_genename_fromgtf.pl genes $output.gene.csv $gtf 0 > $output.gene.name.csv
 $pwd/csv2xlsx.pl -i $output.gene.name.csv -n gene-TPM -i $output.transcript.name.csv -n isoform-TPM -o $output.xlsx
 
 rm *~ #$output.transcript.csv $output.gene.csv
