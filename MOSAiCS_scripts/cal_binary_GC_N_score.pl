aa6826e82eb9312c51f2ca865588ea2a4e9d4d25
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2019-08-06 14:14:00 +0900

    modify mosaics mappability

--- MOSAiCS_scripts/cal_binary_GC_N_score.pl
+++ MOSAiCS_scripts/cal_binary_GC_N_score.pl
@@ -1,7 +1,7 @@
 #############################################################################
 # Data: Aug 26, 2009
-# Author: Pei Fen Kuan 
-# This function converts the fa files to 0(if A or T) and 1(if G or C). First 
+# Author: Pei Fen Kuan
+# This function converts the fa files to 0(if A or T) and 1(if G or C). First
 # download the whole sequence for the chromosome from UCSC.  To use, type
 # perl convert_fa_file_binary_allNuc.pl chrX.fa X 1
 #############################################################################
@@ -35,7 +35,7 @@ open (OUT_N, ">$outfile_N") or  die "cannot open $outfile_N in Step 2\n";
 my $seq;
 while (my $seq_tmp = <IN>) {
   chomp $seq_tmp;
-  $seq = $seq_tmp;	
+  $seq = $seq_tmp;
  }
 close IN;
 system('rm -rf out_tmp.txt');
@@ -47,20 +47,20 @@ sub gc_content {
   my $seq = shift;                        # sequence
   my $win = shift;                        # window
   my $maxID = shift;
-  	
+
   for (my $i = 0; $i <= $maxID; $i++) { # slide across sequence one bp at a time
     my $j = $i*$win;
     my $segment = substr($seq,$j,$win);  # fetch out a segment of the sequence $win bp long starting at $i
     my $g_count = $segment =~ tr/Gg/Gg/;
-    my $segment = substr($seq,$j,$win);
+    $segment = substr($seq,$j,$win);
     my $c_count = $segment =~ tr/Cc/Cc/;
-    my $segment = substr($seq,$j,$win);	
+    $segment = substr($seq,$j,$win);
     my $t_count = $segment =~ tr/Tt/Tt/;
-    my $segment = substr($seq,$j,$win);
+    $segment = substr($seq,$j,$win);
     my $a_count = $segment =~ tr/Aa/Aa/;
-    my $segment = substr($seq,$j,$win);
+    $segment = substr($seq,$j,$win);
     my $gc_count = $segment =~ tr/GCgc/GCgc/;  # trick alert -- see manual entry for tr////
-    my $segment = substr($seq,$j,$win);
+    $segment = substr($seq,$j,$win);
     my $n_count = $segment =~ tr/Nn/Nn/;  # trick alert -- see manual entry for tr////
     #my $gc_content = 100 * $gc_count/length($segment);
     print OUT_GC "$gc_count ";
