409887d1dab54e57f6ddd89689c7659040257866
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-04-23 21:19:39 +0900

    add header command to mapping_QC2.sh

--- mapping_QC2.sh
+++ mapping_QC2.sh
@@ -2,7 +2,7 @@
 cmdname=`basename $0`
 function usage()
 {
-    echo "$cmdname" '[-a] [-b binsize] [-n] [-f of] [-d outputdir] [-p "bowtie2 param"] <exec|stats> <fastq> <prefix> <build>' 1>&2
+    echo "$cmdname" '[-a] [-b binsize] [-n] [-f of] [-d outputdir] [-p "bowtie2 param"] <exec|stats|header> <fastq> <prefix> <build>' 1>&2
 }
 
 pa=""
@@ -76,7 +76,6 @@ if test $type = "exec"; then
     bowtie2.sh -d $cramdir -p "$bowtieparam" "$fastq" $prefix $build
     parse2wig.sh $pa $pair -b $binsize $pens -f $of $cram $head $build
     if test $nopp != 1; then ssp.sh $pair $cram $head $build; fi
-
 elif test $type = "stats"; then
     a=`parsebowtielog2.pl $pair log/bowtie2-$head | grep -v Sample`
     b=`cat log/parsestats-$head.GC.100000 | grep -v Sample | cut -f6,7,8,9`
@@ -88,4 +87,12 @@ elif test $type = "stats"; then
 	echo -en "`tail -n1 sspout/$head.stats.txt | cut -f4,5,6,7,8,9,10,11,12,13,14`"
     fi
     echo ""
+elif test $type = "header"; then
+    a=`parsebowtielog2.pl $pair log/bowtie2-$head | grep Sample`
+    b=`cat log/parsestats-$head.GC.100000 | grep Sample | cut -f6,7,8,9`
+    gcov=`cat log/parsestats-$head.$binsize | grep Sample | cut -f10`
+    b2=`cat log/parsestats-$head.GC.100000 | grep Sample | cut -f11,12`
+    echo -e "$a\t$b\t$gcov\t$b2\t$c"
+#    echo -e "\tSequenced reads	Mapped 1 time	%	Mapped >1 times	%	Mapped all	%	Unmapped	%	Nonredundant	Redundant	Complexity for10M	Read depth	Genome coverage	Tested reads	GC summit	read length	fragment length	SSP-NSC	SSP-RLSC	SSP-RSC	Background uniformity	FCS(read)	FCS(flen)	FCS(1k)	FCS(10k)	FCS(100k)
+"
 fi
