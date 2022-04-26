c90ded02c51c65be5125fde78d28c7e286813857
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-04-26 21:48:06 +0900

    update bowtie2.sh

--- bowtie2.sh
+++ bowtie2.sh
@@ -45,7 +45,7 @@ if test ! -e $bamdir; then mkdir $bamdir; fi
 if test ! -e log; then mkdir log; fi
 
 Ddir=`database.sh`
-bowtie2="singularity exec --bind /work /work/SingularityImages/rnakato_mapping.2022.04.sif bowtie2"
+bowtie2="singularity exec --bind /work,/work2,/work3 /work/SingularityImages/rnakato_mapping.2022.04.sif bowtie2"
 
 file=$bamdir/$prefix$post-$build.sort.cram
 
