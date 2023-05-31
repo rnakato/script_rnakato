9999fac8780bbb93b4e82804c9cfd3c4218e249d
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2023-05-31 11:40:45 +0900

    add execute_command_on_fastq_pairs.sh

--- scatterplot_merge.R
+++
@@ -1,9 +0,0 @@
-# 2列目と3列目で色を変えて重ねた散布図
-
-data <- read.csv("ratio_vs_gd.csv", header=TRUE, sep="\t", quote="")
-
-plot(data[,2], data[,3], ylim=c(0,2), pch="+", col="red")
-par(new=T)
-plot(data[,2], data[,4], ylim=c(0,2), pch="+", col="blue") 
-
-legend("topright", legend=c("ab1220_TT2","ab1220_DKO"), col=c("red","blue"), pch=c("+","+"))
