23a37763646b5dfdb993bc92002825ff132afa3b
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2023-12-14 15:33:50 +0900

    update aspera_ebi.sh fastq_dump.sh

--- aspera_ebi.sh
+++ aspera_ebi.sh
@@ -5,17 +5,16 @@ function usage()
 {
     echo "Usage: ${cmdname} RunTable.txt" 1>&2
     echo '   Options:' 1>&2
-    echo '      -p: paired-end fastq (default: single-end)' 1>&2
+#    echo '      -p: paired-end fastq (default: single-end)' 1>&2
     echo '      -n <int>: column of SRR id (1-based, default: 8)' 1>&2
 }
 
 # check options
 pair=FALSE
 n=8
-while getopts tpn: option
+while getopts n: option
 do
   case ${option} in
-    t) pair=TRUE;;
     n) n=${OPTARG};;
     *)
       usage
@@ -31,22 +30,17 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 file="$1"
-urls=`cut -f$n $file | grep -v fastq_ftp | grep -v submitted_ftp | sed 's|ftp.sra.ebi.ac.uk|era-fasp@fasp.sra.ebi.ac.uk:|g'`
+urls=`cut -f$n $file | grep -v fastq_ftp | grep -v submitted_ftp | sed -e 's|ftp.sra.ebi.ac.uk|era-fasp@fasp.sra.ebi.ac.uk:|g' | sed -e  's|\;| |g'` 
 
 ssh=/home/rnakato/.aspera/connect/etc/asperaweb_id_dsa.openssh
 
 for url in $urls
 do
-    echo $url
-    if test $pair; then
-        ascp -QT -l 100M -P33001 -i $ssh $url ./
+    fq=`basename $url`
+    echo $url $fq
+    if test -e $fq && test -s $fq; then
+        echo "$fq exists"
     else
-        fq1=${id}_1.fastq.gz
-        fq2=${id}_2.fastq.gz
-        if test -e $fq1 && test -s $fq1 && test -e $fq2 && test -s $fq2 ; then
-            echo "$fq1| $fq2 exists"
-        else
-            singularity exec --bind /home,/work,/work2 /work/SingularityImages/SRAtools.3.0.0.sif fastq-dump --gzip $id
-        fi
+        ascp -QT -l 100M -P33001 -i $ssh $url ./
     fi
 done
