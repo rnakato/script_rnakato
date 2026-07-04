5647e881e67725cbcfe4d9690215ccae26d45b08
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-07-04 14:18:27 +0900

    Update version 3.5.1

--- download_accession.sh
+++ download_accession.sh
@@ -19,7 +19,7 @@ acc=$1 #PRJNA1445099
 ex "mkdir -p metadata"
 
 ex "curl -L --fail --retry 5 --retry-delay 5 \
-  "https://www.ebi.ac.uk/ena/portal/api/filereport?accession=${acc}&result=read_run&fields=run_accession,study_accession,secondary_study_accession,experiment_accession,sample_accession,secondary_sample_accession,scientific_name,library_strategy,library_layout,read_count,base_count,fastq_ftp,sra_ftp&format=tsv&download=true" \
+  \"https://www.ebi.ac.uk/ena/portal/api/filereport?accession=${acc}&result=read_run&fields=run_accession,study_accession,secondary_study_accession,experiment_accession,sample_accession,secondary_sample_accession,scientific_name,library_strategy,library_layout,read_count,base_count,fastq_ftp,sra_ftp&format=tsv&download=true\" \
   -o metadata/${acc}.ena.runinfo.tsv"
 
 ex "tail -n +2 metadata/${acc}.ena.runinfo.tsv \
